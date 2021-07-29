81dd688d943ab250b45f68391f812915de9fe325
Katelyn Shepard <katelyn.shepard@gmail.com>
2021-07-29 09:47:27 -0400

    Add Image URL String

--- src/main/java/com/cygnus/bookface/BookFaceClass.java
+++ src/main/java/com/cygnus/bookface/BookFaceClass.java
@@ -14,23 +14,27 @@ public class BookFaceClass {
     private String title;
     private String author;
     private Long isbn;
+    private String imageURL;
 
     public BookFaceClass() {
         this.title = null;
         this.author = null;
         this.isbn = null;
+        this.imageURL = null;
     }
 
-    public BookFaceClass(String title, String author, Long isbn) {
+    public BookFaceClass(String title, String author, Long isbn, String imageURL) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.imageURL = imageURL;
     }
 
     public BookFaceClass(BookFaceClass other, Long id) {
         this.title = other.getTitle();
         this.author = other.getAuthor();
-        this.isbn = other.geIsbn();
+        this.isbn = other.getIsbn();
+        this.imageURL = other.getImageURL();
         this.id = id;
     }
 
@@ -54,11 +58,19 @@ public class BookFaceClass {
         this.author = author;
     }
 
-    public Long geIsbn() {
+    public Long getIsbn() {
         return this.isbn;
     }
 
     public void setIsbn(Long isbn) {
         this.isbn = isbn;
+    }
+
+    public String getImageURL() {
+        return this.imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 }
