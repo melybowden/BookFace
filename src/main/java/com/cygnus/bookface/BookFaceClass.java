d8c170a1baec108fb7f9b97f331591abd3bfce0a
Katelyn Shepard <katelyn.shepard@gmail.com>
2021-07-29 11:16:06 -0400

    Add Year

--- src/main/java/com/cygnus/bookface/BookFaceClass.java
+++ src/main/java/com/cygnus/bookface/BookFaceClass.java
@@ -11,35 +11,48 @@ public class BookFaceClass {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String imageURL;
     private String title;
     private String author;
+    private Long year;
     private Long isbn;
-    private String imageURL;
 
     public BookFaceClass() {
+        this.imageURL = null;
         this.title = null;
         this.author = null;
+        this.year = null;
         this.isbn = null;
-        this.imageURL = null;
     }
 
-    public BookFaceClass(String title, String author, Long isbn, String imageURL) {
+    public BookFaceClass(String imageURL, String title, String author, Long year, Long isbn) {
+        this.imageURL = imageURL;
         this.title = title;
         this.author = author;
+        this.year = year;
         this.isbn = isbn;
-        this.imageURL = imageURL;
     }
 
     public BookFaceClass(BookFaceClass other, Long id) {
+        this.imageURL = other.getImageURL();
         this.title = other.getTitle();
         this.author = other.getAuthor();
+        this.year = other.getYear();
         this.isbn = other.getIsbn();
-        this.imageURL = other.getImageURL();
         this.id = id;
     }
 
     public Long getID() {
         return this.id;
+    }
+
+    
+    public String getImageURL() {
+        return this.imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 
     public String getTitle() {
@@ -58,19 +71,19 @@ public class BookFaceClass {
         this.author = author;
     }
 
+    public Long getYear() {
+        return this.year;
+    }
+
+    public void setYear(Long year) {
+        this.year = year;
+    }
+
     public Long getIsbn() {
         return this.isbn;
     }
 
     public void setIsbn(Long isbn) {
         this.isbn = isbn;
-    }
-
-    public String getImageURL() {
-        return this.imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
     }
 }
