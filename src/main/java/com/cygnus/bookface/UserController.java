7a984dc4a8fd46989e9fdeed19c0d7874f232327
Katelyn Shepard <katelyn.shepard@gmail.com>
2021-08-03 15:34:40 -0400

    Add get request for user logins

--- src/main/java/com/cygnus/bookface/UserController.java
+++ src/main/java/com/cygnus/bookface/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 // import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,11 @@ public class UserController {
         }
         userRepository.save(user);
         return Pair.of("User succesfully created", user);
+    }
+
+    @GetMapping("/users/register")
+    public List<UserClass> getAllUsers() {
+        return userRepository.findAll();
     }
 
     @PostMapping("/users/login")
